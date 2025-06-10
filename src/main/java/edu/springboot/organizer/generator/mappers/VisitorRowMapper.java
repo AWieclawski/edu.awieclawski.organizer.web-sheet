520fba32b30f67074411a761125e1c68aeb383b4
Andrzej Więcławski <a_wieclawski@poczta.pl>
2025-06-10 23:01:09 +0200

    upd VisitorRowMapper

--- src/main/java/edu/springboot/organizer/generator/mappers/VisitorRowMapper.java
+++ src/main/java/edu/springboot/organizer/generator/mappers/VisitorRowMapper.java
@@ -3,6 +3,7 @@ package edu.springboot.organizer.generator.mappers;
 import edu.springboot.organizer.data.models.Visitor;
 import edu.springboot.organizer.generator.dtos.VisitorDto;
 import edu.springboot.organizer.utils.DateUtils;
+import lombok.var;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -12,13 +13,15 @@ public class VisitorRowMapper implements RowMapper<VisitorDto> {
 
     @Override
     public VisitorDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return VisitorDto.builder()
+        var dto = VisitorDto.builder()
                 .name(rs.getString(Visitor.Const.NAME.getColumn()))
                 .url(rs.getString(Visitor.Const.URL.getColumn()))
                 .ip(rs.getString(Visitor.Const.IP.getColumn()))
                 .timestamp(DateUtils.timestampToToString(rs.getTimestamp(Visitor.Const.TIMESTAMP.getColumn())))
                 .id(rs.getString(Visitor.BaseConst.ID.getColumn()))
                 .build();
+        dto.hashCode();
+        return dto;
     }
 
 
