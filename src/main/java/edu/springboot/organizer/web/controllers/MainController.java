950c5cc202f979a8791797589c26b82233d8c0c2
Andrzej Więcławski <a_wieclawski@poczta.pl>
2025-11-09 11:20:38 +0100

    upd prod env variables

--- src/main/java/edu/springboot/organizer/web/controllers/MainController.java
+++ src/main/java/edu/springboot/organizer/web/controllers/MainController.java
@@ -3,12 +3,14 @@ package edu.springboot.organizer.web.controllers;
 import edu.springboot.organizer.web.controllers.forms.PickDateFormController;
 import edu.springboot.organizer.web.exceptions.ControllerException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequestMapping()
 @RequiredArgsConstructor
@@ -17,8 +19,9 @@ public class MainController {
     @Value("${endpoint.pick-date}")
     private String viewRedirect;
 
-    @GetMapping(path = {"", "/home"})
+    @GetMapping(path = {"","/", "/home"})
     public ModelAndView veryStart() {
+        log.info("Root endpoint [GET] received");
         return new ModelAndView("welcome");
     }
 
