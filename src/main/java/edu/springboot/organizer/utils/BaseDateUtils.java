59f57c6ef829a447f74af8c62200fe81a8fecdb6
Andrzej Więcławski <45081296+AWieclawski@users.noreply.github.com>
2026-05-02 08:56:28 +0200

    Update BaseDateUtils.java
    add getRandomSuffix

--- src/main/java/edu/springboot/organizer/utils/BaseDateUtils.java
+++ src/main/java/edu/springboot/organizer/utils/BaseDateUtils.java
@@ -10,14 +10,28 @@ import java.time.format.DateTimeFormatter;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BaseDateUtils {
 
-    private static final String PATTERN = "yyyyMMddHHmmssSSSSS";
+    private static final String PATTERN = "yyyyMMddHHmmssSSS";
+
+    private final static String ZONE_ID = "Europe/Warsaw";
 
     public static String getBaseTimestampId() {
         LocalDateTime now = LocalDateTime.now();
-        return now.format(getBaseFormatter());
+        return now.format(getBaseFormatter()) + getRandomSuffix();
     }
 
     private static DateTimeFormatter getBaseFormatter() {
-        return DateTimeFormatter.ofPattern(PATTERN).withZone(ZoneId.systemDefault());
+        return DateTimeFormatter.ofPattern(PATTERN).withZone(getZoneIdi());
     }
+
+    private static ZoneId getZoneIdi() {
+        return ZoneId.of(ZONE_ID);
+    }
+
+    private static int getRandomSuffix() {
+        final int MIN_ID = 1000;
+        final int MAX_ID = 9999;
+        final int RANGE = MAX_ID - MIN_ID + 1;
+        return new Random().nextInt(RANGE) + MIN_ID;
+    }
+
 }
