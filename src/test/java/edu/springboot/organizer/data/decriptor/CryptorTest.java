309aea8a31fdb51894a534fa60ad8f974c3cfb6e
Andrzej Więcławski <45081296+AWieclawski@users.noreply.github.com>
2026-05-02 08:55:32 +0200

    Update CryptorTest.java
    upd logging

--- src/test/java/edu/springboot/organizer/data/decriptor/CryptorTest.java
+++ src/test/java/edu/springboot/organizer/data/decriptor/CryptorTest.java
@@ -1,36 +1,50 @@
 package edu.springboot.organizer.data.decriptor;
 
 import edu.springboot.organizer.utils.BaseDateUtils;
-import org.junit.jupiter.api.Assertions;
+import edu.springboot.organizer.utils.BaseStringUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 class CryptorTest {
 
     @Test
     void cratedDifferentNumbersFromGeneratedStringFromDates() {
-        List<Integer> testIntList = new ArrayList<>();
-        List<String> testStrList = new ArrayList<>();
+        List<Integer> testHashIdList = new ArrayList<>();
+        List<String> testTimestampIdList = new ArrayList<>();
         for (int i = 0; i < 9999; i++) {
-            String timeStampId = BaseDateUtils.getBaseTimestampId();
-            int hashId = assignHashId(testIntList, Cryptor.encryptWord(timeStampId), i, timeStampId);
-            Assertions.assertFalse(testStrList.contains(timeStampId), (String.format("%4d - found: %s", i, timeStampId)));
-            testIntList.add(hashId);
-            testStrList.add(timeStampId);
+            String baseTimestampId = assignTimestampId(testHashIdList, testTimestampIdList, BaseDateUtils.getBaseTimestampId(), i);
+            String counter = getConterAsString(i);
+            log.info("{} => assigned baseTimestampId: {}", counter, baseTimestampId);
         }
     }
 
     private int assignHashId(List<Integer> testIntList, int hashId, int i, String timeStampId) {
-        try {
-            Assertions.assertFalse(testIntList.contains(hashId));
-        } catch (Throwable th) {
-            String counter = String.format("%3d", i);
-            System.out.println((counter + "[" + timeStampId + "] - found: " + hashId + " replaced by: " + ++hashId));
-            assignHashId(testIntList, hashId, i, timeStampId);
+        while (testIntList.contains(i)) {
+            String counter = getConterAsString(i);
+            log.warn("{}. for [{}] - found HashId [{}] is duplicated , but replaced by: {}", counter, timeStampId, hashId, ++hashId);
         }
         return hashId;
+    }
+
+    private String assignTimestampId(List<Integer> testIntList, List<String> testStrList, String timeStampId, int i) {
+        while (testStrList.contains(timeStampId)) {
+            String counter = getConterAsString(i);
+            String incrementedTimestampId = BaseStringUtils.replaceLastDigitsIncreasedByOne(timeStampId);
+            log.warn("{}. found TimeStampId [{}] is duplicated, but replaced by: {}", counter, timeStampId, incrementedTimestampId);
+            timeStampId = incrementedTimestampId;
+        }
+        int hashId = assignHashId(testIntList, Cryptor.encryptWord(timeStampId), i, timeStampId);
+        testIntList.add(hashId);
+        testStrList.add(timeStampId);
+        return timeStampId;
+    }
+
+    private String getConterAsString(int i) {
+        return String.format("%04d", i);
     }
 
 }
